045968b7e9f65c8d0ceeb245c92a80e9390d697d
xuanduc97uet <xuanduc1124@gmail.com>
2016-09-05 17:13:38 +0700

    fix typo 2

--- app/scripts/typo-ui.js
+++ app/scripts/typo-ui.js
@@ -63,3 +63,58 @@ function checkSpell(input) {
 		errorCount : errorCount
 	}
 };
+
+function fixSpell(input) {
+  //input = input.trim();
+
+  var words = [];
+	var errorCount = 0;
+
+  var l = -1,
+    r = 0;
+
+  for (r in input) {
+    //console.log(r);
+    //console.log(input[r]);
+    if (!isAlphabet(input.charCodeAt(r))) {
+      if (l != -1) {
+        words.push(input.substr(l, r - l));
+        l = -1;
+      };
+      words.push(input[r]);
+    } else {
+      if (l == -1) l = r;
+    };
+
+  };
+  //console.log(words);
+  if (l != -1) {
+    words.push(input.substr(l));
+    l = -1;
+  };
+
+  var suggestWords = [];
+
+  for (var i in words) {
+    //console.log(i);
+    if (!isAlphabet(words[i].charCodeAt(0))) {
+      if (words[i] === '\n') {
+				words[i] = '<br/>';
+			}
+			suggestWords.push(words[i]);
+    } else {
+      if (dictionary.check(words[i])) {
+        suggestWords.push(words[i]);
+      } else {
+        suggestWords.push(dictionary.suggest(words[i]);
+				errorCount++;
+      };
+    };
+    //console.log(suggestWords[i]);
+  };
+  //console.log(suggestWords);
+  return {
+		suggestWords : suggestWords.join(""),
+		errorCount : errorCount
+	}
+};
